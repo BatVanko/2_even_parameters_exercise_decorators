fa55cc6b5cbd70e97914233a0f6755542096929a
BatVanko <71530199+BatVanko@users.noreply.github.com>
2023-06-29 23:08:23 +0300

    Add files via upload

---
+++ 2_even_parameters.py
@@ -0,0 +1,17 @@
+def even_parameters(func):
+    def wrapper(*args):
+        for num in args:
+            if not isinstance(num, int) or num % 2 != 0:
+                return "Please use only even numbers!"
+        return func(*args)
+    return wrapper
+
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+
+print(add(2, 4))
+print(add("Peter", 1))
+a = 6
